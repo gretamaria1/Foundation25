e224c1b4bde0405fb77d69f8b0ef617d82eb43d0
gretamaria1 <229802785+gretamaria1@users.noreply.github.com>
2025-09-29 19:53:32 +0300

    Lab7

---
+++ Lab7/script.js
@@ -0,0 +1,30 @@
+let firstName = "Mohammed";
+
+let newSentence = "Hello my name is " + firstName;
+
+// console.log(newSentence)
+
+// console.log("Hello, I'm " + firstName + "I am called " + firstName + "call me " + firstName);
+
+console.log(`Hello, I'm ${firstName}, I am called ${firstName}, call me ${firstName}`);
+
+// alert("Hi how are you!")
+let num1;
+const num2 = 5;
+
+num1 = 10;
+
+let result = num1;
+console.log(result);
+
+let mixedResult = firstName + num1;
+console.log(mixedResult);
+
+let a = 10;
+let b = 3;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);

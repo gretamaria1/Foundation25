af65332c8c90c719b69bfd34b4f82f6c8a375467
gretamaria1 <229802785+gretamaria1@users.noreply.github.com>
2025-10-06 19:47:54 +0300

    Lab8

---
+++ Lab8/script.js
@@ -0,0 +1,24 @@
+let grades = [50,89,75,84,99];
+let sum = 0;
+for (let i = 0; i<5; i++) 
+{
+    sum += grades[i];
+}
+
+console.log(sum);
+let result = sum / 5;
+console.log(result);
+
+/*
+//Grade Calculator
+const grade = prompt("Enter the grade");
+console.log(grade);
+
+if (grade >= 90) {
+    console.log("A");
+} else if (grade >= 80) {
+    console.log("B");
+} else if (grade >= 60) {
+    console.log("C");
+} 
+*/

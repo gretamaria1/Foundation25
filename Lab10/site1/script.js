5ee15083a2b2a2a1f2875d9fc1919260af575c94
gretamaria1 <229802785+gretamaria1@users.noreply.github.com>
2025-10-13 19:57:07 +0300

    Lab10

---
+++ Lab10/site1/script.js
@@ -0,0 +1,31 @@
+const daysElement = document.getElementById("days");
+const hoursElement = document.querySelector("#hours");
+const minutesElement = document.querySelector("#minutes");
+const secondsElement = document.querySelector("#seconds");
+
+const endDate = new Date(2026,0,1);
+//console.log(endYear);
+
+function countDown() {
+    const currentDate = new Date();
+    //console.log(currentDate.getTime()/1000);
+
+    const totalSeconds = (endDate - currentDate)/1000;
+    console.log(totalSeconds);
+
+    let days = Math.floor(totalSeconds/(24*3600));
+    let hours = Math.floor(totalSeconds%(24*3600)/3600);
+    let minutes = Math.floor(totalSeconds%(24*3600)%3600/60);
+    let seconds = Math.floor(totalSeconds%(24*3600)%3600%60);
+
+    console.log(days, hours, minutes, seconds);
+
+    daysElement.innerHTML = days;
+    hoursElement.innerHTML = hours;
+    minutesElement.innerHTML = minutes;
+    secondsElement.innerHTML = seconds;
+}
+
+countDown();
+
+setInterval(countDown, 1000);
---
+++ Lab10/site2/color.js
@@ -0,0 +1,21 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025", "#f125aaff", "#36f125ff"];
+
+const btn1 = document.querySelector(".btn1");
+const btn2 = document.querySelector(".btn2");
+const btn3 = document.querySelector(".btn3");
+
+const colorText = document.querySelector("#colorCode");
+const colorPanel = document.querySelector("#colorPanel");
+
+function randomColor() 
+{
+    console.log('First button got clicked!');
+    let colorIndex = Math.floor(Math.random()*colors.length);
+    console.log(colorIndex);
+    colorPanel.style.backgroundColor = colors[colorIndex];
+    colorText.innerText = colors[colorIndex];
+}
+btn1.addEventListener("click", randomColor);
+
+// Use this format to change the colors rgba(133,122,200)
+// rgba (r,g,b) 0 <= r <= 255, 0 <= g <= 255, 0 <= b <= 255

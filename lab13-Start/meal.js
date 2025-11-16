7efcdc2e28087e6be1538ddac980692d7b9d5463
gretamaria1 <229802785+gretamaria1@users.noreply.github.com>
2025-11-16 12:06:32 +0200

    Lab13 - HW9
    Work on Fetch & LocalStorage

---
+++ lab13-Start/meal.js
@@ -0,0 +1,79 @@
+//1- Link to get a random meal
+//https://www.themealdb.com/api/json/v1/1/random.php
+
+//2- Link to lookup a specific meal with an id
+//https://www.themealdb.com/api/json/v1/1/lookup.php?i=
+
+//3- Link to search for meals using a keyword
+//https://www.themealdb.com/api/json/v1/1/search.php?s=
+
+const mealsElement = document.getElementById('meals');
+getRandomMeal();
+
+async function getRandomMeal() 
+{
+    const resp = await fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+    
+    const randomData = await resp.json();
+    
+    const randomMeal = randomData.meals[0];
+    console.log(randomMeal);
+
+    mealsElement.innerHTML = '';
+    addMeal(randomMeal);
+
+}
+
+function addMeal(mealData)
+{
+    const meal = document.createElement('div');
+    meal.classList.add('meal');
+
+    meal.innerHTML =    `<div class="meal-header">
+                            <span class="random">Meal of the Day</span>
+                            <img src="${mealData.strMealThumb}" alt="${mealData.strMeal}">
+                        </div>
+                        <div class="meal-body">
+                            <h3>${mealData.strMeal}</h3>
+                            <button class="fav-btn">
+                                <i class="fas fa-heart"></i>
+                            </button>
+                        </div>`;
+
+    let favoriteButton = meal.querySelector('.fav-btn');
+    favoriteButton.addEventListener('click', () => {
+        if (favoriteButton.classList.contains('active'))
+        {
+            favoriteButton.classList.remove('active');
+            removeMealFromLocalStorage(mealData.idMeal);
+        }
+        else
+        {
+            favoriteButton.classList.add('active');
+            addMealToLocalStorage(mealData.idMeal);
+        }
+    })    
+
+    mealsElement.appendChild(meal);
+}
+
+function addMealToLocalStorage(mealId)
+{
+    const mealIds = getMealsFromLocalStorage();
+    localStorage.setItem('mealId', JSON.stringify([...mealIds,mealId]));
+}
+
+function removeMealFromLocalStorage(mealId)
+{
+    const mealIds = getMealsFromLocalStorage();
+    localStorage.setItem('mealId', JSON.stringify(
+        mealIds.filter(id => id !=mealId)
+    ));
+}
+
+function getMealsFromLocalStorage()
+{
+    const mealIds = JSON.parse(localStorage.getItem('mealId'));
+
+    return mealIds === null ? [] : mealIds;
+}

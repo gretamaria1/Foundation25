d1d3144396369442739b7c2a49a3cfcfc20e7c3f
gretamaria1 <229802785+gretamaria1@users.noreply.github.com>
2025-11-17 19:51:16 +0200

    Lab13
    Class work with APIs and favorites

--- lab13-Start/meal.js
+++ lab13-Start/meal.js
@@ -8,6 +8,7 @@
 //https://www.themealdb.com/api/json/v1/1/search.php?s=
 
 const mealsElement = document.getElementById('meals');
+const favorites = document.querySelector('.favorites');
 getRandomMeal();
 
 async function getRandomMeal() 
@@ -52,9 +53,12 @@ function addMeal(mealData)
             favoriteButton.classList.add('active');
             addMealToLocalStorage(mealData.idMeal);
         }
+        updateFavoriteMeals();
     })    
 
     mealsElement.appendChild(meal);
+
+    updateFavoriteMeals();
 }
 
 function addMealToLocalStorage(mealId)
@@ -77,3 +81,45 @@ function getMealsFromLocalStorage()
 
     return mealIds === null ? [] : mealIds;
 }
+
+const updateFavoriteMeals = () => {
+    favorites.innerHTML = '';
+    const mealIds = getMealsFromLocalStorage();
+    console.log(mealIds);
+
+    let meals = [];
+    mealIds.forEach(async (meal) => {
+        let tmpMeal = await getMealById(meal);
+        meals.push(tmpMeal);
+
+        addMealToFavorites(tmpMeal);
+    })
+}
+
+const getMealById = async (id) => {
+    const resp = await fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i=' + id);
+    const mealData = await resp.json();
+    const meal = mealData.meals[0];
+    console.log(meal);
+
+    return meal;
+}
+
+const addMealToFavorites = (meal) => {
+    const favoriteMeal = document.createElement('li');
+    favoriteMeal.innerHTML = `
+                            <img id="fav-img" 
+                                src="${meal.strMealThumb}" 
+                                alt="${meal.strMeal}">
+                            <span>${meal.strMeal}</span>
+                            <button class="clear">
+                            <i class="fas fa-window-close"></i>
+                            </button>`;
+    const clearButton = favoriteMeal.querySelector('.clear');
+    clearButton.addEventListener('click', () => {
+        removeMealFromLocalStorage(meal.idMeal);
+        updateFavoriteMeals();
+    })
+    favorites.appendChild(favoriteMeal);
+}
+

d16767fc0b4c402c20f904a5b0a352f4212f3278
gretamaria1 <229802785+gretamaria1@users.noreply.github.com>
2025-11-24 19:52:44 +0200

    Lab 16 - Firebase

---
+++ Lab16/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCEapodZ_yaV_2ZwYHUKyNCTiO2-IB-Hkk",
+    authDomain: "testingfirebase-6cb89.firebaseapp.com",
+    projectId: "testingfirebase-6cb89",
+    storageBucket: "testingfirebase-6cb89.firebasestorage.app",
+    messagingSenderId: "584730979222",
+    appId: "1:584730979222:web:0bcb420d7aac3f8d664bb7"
+  };

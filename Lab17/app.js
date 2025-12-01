3c4e05e7a6be0d0ca4b3f245079b09260c13ed1a
gretamaria1 <229802785+gretamaria1@users.noreply.github.com>
2025-12-01 19:55:19 +0200

    Lab17

---
+++ Lab17/app.js
@@ -0,0 +1,18 @@
+import {logout} from './auth.js';
+import { readEntries } from './firestore.js';
+
+const logoutBtn = document.querySelector("#logout");
+const messageDiv = document.getElementById("message");
+
+logoutBtn.addEventListener('click', async ()=> {
+    try {
+        await logout();
+        window.location.href = "index.html";
+    }
+    catch (error) {
+        console.error("Logout failed", error);
+        messageDiv.textContent = "Logout failed: " + error.message;
+    }
+})
+
+const results = await readEntries ();
